5bd7341ccae146b403c14b51eed8887d372620f9
Markus <markus@easy-lms.com>
2023-06-02 16:57:14 +0200

    test deploy

--- templates/Articles/index.php
+++ templates/Articles/index.php
@@ -1,6 +1,6 @@
 <!-- File: src/Template/Articles/index.php -->
 <?= $this->Html->link('Add Article', ['action' => 'add']) ?>
-<h1>Articles</h1>
+<h1>Articles (Updated)</h1>
 <table>
     <tr>
         <th>Title</th>
